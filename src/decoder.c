604fdca8865b4c5c07a8e3fcfa3c4e4ba677e685
BigNocciolino <alfonsosorrentino211@gmail.com>
2022-06-15 23:42:50 +0200

    implement gpx writer to file

--- src/decoder.c
+++ src/decoder.c
@@ -21,11 +21,26 @@ int main(int argc, char **argv) {
 
     char *file_path = NULL;
     char *xml_path = NULL;
-    FILE *nema_file;
+    FILE *nema_file, *xml_file;
     struct GPRMC rmc;
+
+    // XMl nodes
+    mxml_node_t *xml;
+    mxml_node_t *gpx;
+    mxml_node_t *trk;
+    mxml_node_t *trkseg;
+    mxml_node_t *trkpt;
+    mxml_node_t *time;
+
+    xml = mxmlNewXML("1.0");
+    gpx = mxmlNewElement(xml, "gpx");
+        trk = mxmlNewElement(gpx, "trk");
+            trkseg = mxmlNewElement(trk, "trkseg");
+
     
-    if (argc > 1) {
+    if (argc > 2) {
        file_path = argv[1];
+       xml_path = argv[2];
     }else {
         printf("Usage:\n./decoder [file]\n");
         exit(1);
@@ -40,6 +55,7 @@ int main(int argc, char **argv) {
     // Read the content
     char sentence[SENT_MAX_LEN];
     char sent_id[NEMA_ID_LEN];
+    char element[MAX_ELEMENT_LEN];
 
     while(fgets(sentence, sizeof(sentence), nema_file) != NULL) {
         if (sentence[0] == '$') {
@@ -48,10 +64,27 @@ int main(int argc, char **argv) {
             if (strcmp(sent_id, "$GPRMC") == 0) {
                 // No newline char sience the sentece have it
                 parse_rmc(&rmc, sentence);
-                print_rmc(&rmc);
+                trkpt = mxmlNewElement(trkseg, "trkpt");
+                memset(element, 0, sizeof(element));
+                sprintf(element, "%f", rmc.lat.latitude);
+                mxmlElementSetAttr(trkpt, "lat", element);
+                memset(element, 0, sizeof(element));
+                sprintf(element, "%f", rmc.lng.longitude);
+                mxmlElementSetAttr(trkpt, "lon", element);
+                time = mxmlNewElement(trkseg, "time");
+                mxmlNewText(time, 0, rmc.time);
             }
         } 
     } 
+
+    xml_file = fopen(xml_path, "w");
+    if (!xml_file) {
+        printf("Could not open gpx file %s\n", xml_path);
+        // mabye exit
+    }else {
+        mxmlSaveFile(xml, xml_file, MXML_NO_CALLBACK);
+        fclose(xml_file);
+    }
 
     fclose(nema_file);
 
