abfa0acc8122275265984505ae20408f4bd6b2c6
BigNocciolino <alfonsosorrentino211@gmail.com>
2022-07-03 16:28:17 +0200

    make buffer bigger according to compilation warnings

--- src/utils.c
+++ src/utils.c
@@ -4,7 +4,7 @@
 #include <nema_parser.h>
 
 char *convert_to_ISO(struct Date *d, struct Time *t) {
-    char *iso_timestamp = malloc(sizeof(char) * 25); 
+    char *iso_timestamp = malloc(sizeof(char) * 35); 
     sprintf(iso_timestamp, "%d-%d-%dT%d:%d:%d+00Z", d->year, d->month, d->day, t->hour, t->minutes, t->seconds);
     return iso_timestamp;
 }
