fade94088816872295650868a63d7e5f14433f44
BigNocciolino <alfonsosorrentino211@gmail.com>
2022-09-08 14:10:56 +0200

    implement new degrees_to_decimal

--- src/util.c
+++ src/util.c
@@ -1,5 +1,26 @@
 #include "util.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <nmea.h>
 
 float kn_to_ms(float kn) {
     return kn * MS_CONVERSION;
+}
+
+float degrees_to_decimal(int degrees, double minutes, char cardinal, uint8_t type) {
+    // Decimal = DEgress + Mnutes/60 + Seconds/3600
+    float decimal = 0;
+    minutes = minutes / 60;
+    decimal = minutes + degrees;
+    
+    if (type == LAT_TYPE) {
+        if (cardinal == NMEA_CARDINAL_DIR_SOUTH) {
+            decimal *= -1;
+        }
+    }else if (type == LON_TYPE) {
+        if (cardinal == NMEA_CARDINAL_DIR_WEST ) {
+            decimal *= -1;
+        }
+    }
+    return decimal; 
 }
