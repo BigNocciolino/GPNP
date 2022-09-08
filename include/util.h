09c06220c59c310fb173f3f3327d4738904b6469
BigNocciolino <alfonsosorrentino211@gmail.com>
2022-09-08 14:09:59 +0200

    add function to convert degrees and minutes into decimal format

--- include/util.h
+++ include/util.h
@@ -1,8 +1,12 @@
 #ifndef UTIL_H
 #define UTIL_H
+#include <stdint.h>
 
 #define MS_CONVERSION 0.514444
+#define LON_TYPE 1
+#define LAT_TYPE 0
 
 float kn_to_ms(float kn);
+float degrees_to_decimal(int degrees, double minutes, char cardinal, uint8_t type);
 
 #endif
