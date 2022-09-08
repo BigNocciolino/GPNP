6136f2226e1a8c0f1874293e9935645a87f1eb7a
BigNocciolino <alfonsosorrentino211@gmail.com>
2022-09-08 14:33:18 +0200

    moved to the new nmea parser library

--- src/gpx_creator.c
+++ src/gpx_creator.c
@@ -29,6 +29,9 @@ int write_to_file(char *nema_file_path, char *gpx_file_path) {
     mxml_node_t *elevation;
     mxml_node_t *satellites;
 
+    int sentences = 0;
+    int failed_sents = 0;
+
     // start the build of the gpx file
     xml = mxmlNewXML("1.0");
     gpx = mxmlNewElement(xml, "gpx");
@@ -43,41 +46,51 @@ int write_to_file(char *nema_file_path, char *gpx_file_path) {
     } 
 
     // Read the content
-    char sentence[NMEA_MAX_LENGTH] = {0};
+    char sentence[NMEA_MAX_LENGTH+1] = {0};
     char timestamp[35] = {0};
 
+    // FIXME library return wrong values
     while(fgets(sentence, sizeof(sentence), nema_file) != NULL) {
-        data = nmea_parse(sentence, strlen(sentence), 0, 0);
+        sentences++;
+        data = nmea_parse(sentence, strlen(sentence), 1, 0);
         if (data != NULL) {
             if (NMEA_GPRMC == data->type) {
                 nmea_gprmc_s *rmc = (nmea_gprmc_s *) data;
                 // RMC
-                trkpt = mxmlNewElement(trkseg, "trkpt");
-                // Set all the attribute for trkpt
-                mxmlElementSetAttrf(trkpt, "lat", "%f", rmc->latitude); 
-                mxmlElementSetAttrf(trkpt, "lon", "%f", rmc->longitude);
-                // Set time element
-                time = mxmlNewElement(trkpt, "time");
-                sprintf(timestamp, "%d-%d-%dT%d:%d:%d+00Z", rmc->date_time.tm_year, rmc->date_time.tm_mon, rmc->date_time.tm_mday,
-                                                            rmc->date_time.tm_hour, rmc->date_time.tm_min, rmc->date_time.tm_sec);
-                mxmlNewText(time, 0, timestamp);
-                // Store the speed
-                speed = mxmlNewElement(trkpt, "speed");
-                mxmlNewTextf(speed, 0, "%f", kn_to_ms(rmc->gndspd_knots));
+                if (rmc->valid) {
+                    trkpt = mxmlNewElement(trkseg, "trkpt");
+                    // Set all the attribute for trkpt
+                    mxmlElementSetAttrf(trkpt, "lat", "%f", degrees_to_decimal(rmc->latitude.degrees, rmc->latitude.minutes, (char)rmc->latitude.cardinal, LAT_TYPE)); 
+                    mxmlElementSetAttrf(trkpt, "lon", "%f", degrees_to_decimal(rmc->longitude.degrees, rmc->longitude.minutes, (char)rmc->longitude.cardinal, LON_TYPE));
+                    // Set time elemente
+                    time = mxmlNewElement(trkpt, "time");
+                    sprintf(timestamp, "%4d-%02d-%02dT%02d:%02d:%02d.000Z", (rmc->date_time.tm_year+1900), (rmc->date_time.tm_mon+1), rmc->date_time.tm_mday,
+                                                                rmc->date_time.tm_hour, rmc->date_time.tm_min, rmc->date_time.tm_sec);
+                    mxmlNewText(time, 0, timestamp);
+                    // Store the speed
+                    speed = mxmlNewElement(trkpt, "speed");
+                    mxmlNewTextf(speed, 0, "%f", kn_to_ms(rmc->gndspd_knots));
+                }
             }
             if (NMEA_GPGGA == data->type) { 
                 nmea_gpgga_s *gga = (nmea_gpgga_s *) data;
                 // GGA
-                elevation = mxmlNewElement(trkpt, "ele");
-                mxmlNewTextf(elevation, 0, "%f",gga->altitude);
+                if (trkpt != NULL) {
+                    if (gga->n_satellites > 3) {
+                        elevation = mxmlNewElement(trkpt, "ele");
+                        mxmlNewTextf(elevation, 0, "%f",gga->altitude);
 
-                satellites = mxmlNewElement(trkpt, "sat");
-                mxmlNewTextf(satellites, 0, "%d", gga->n_satellites);
+                        satellites = mxmlNewElement(trkpt, "sat");
+                        mxmlNewTextf(satellites, 0, "%d", gga->n_satellites);
+                    }
+                }
             }
         }else {
-            printf("Failed to parse sentence\n");
+            failed_sents++;
         }
     }
+
+    printf("Failed to parse %d sentences, on %d sentences\n", failed_sents, sentences);
 
     gpx_file = fopen(gpx_file_path, "w");
     if (gpx_file) {
